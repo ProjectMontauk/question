15611cb28f0abef348da1ca64e8c32b72db18f2f
TrevorGillan <tfg35@cornell.edu>
2025-07-18 09:43:10 -0400

    should deploy deposit loading...

--- src/components/AutoDeposit.tsx
+++ src/components/AutoDeposit.tsx
@@ -37,7 +37,7 @@ export default function AutoDeposit() {
         onSuccess: () => {
           refetch();
         },
-        onError: (error) => {
+        onError: () => {
           hasAttemptedMint.current = false;
         }
       });
